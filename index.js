d6b1a5a1737b63430f108fd971fe4f9859e3419c
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-07-24 23:14:38 -0400

    initial commit finished db.find handler and setting uo project

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db')
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (request, response) => {
+
+response.send('hello world from express');
+
+});
+
+server.get('/now', (request, response) =>{
+const now = new Date().toISOString();
+    response.send(now);
+    
+    });
+
+    server.get('/users', (request, response) => {
+        db.find()
+        .then(users => {
+            response.status(200).json(users);
+        })
+        .catch(err => {
+            response.status(500).json({success:false,err})
+        });
+
+ })
+
+
+
+ 
+
+server.listen(4000, ()=> {
+
+console.log('server listening on port 4000');
+
+
+});
