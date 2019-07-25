a25d4249dec0be04f1ed0c4b88d3042ce79b46ec
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-07-25 01:50:40 -0400

    mvp complete having trouble with update

--- index.js
+++ index.js
@@ -26,11 +26,79 @@ const now = new Date().toISOString();
             response.status(500).json({success:false,err})
         });
 
- })
+ });
 
 
+
+ server.post('/users',(request, response) => {
+            const userInfo = request.body;
+            db.insert(userInfo)
+            .then(user => {
+                response.status(201).json({success:true, user});
+            })
+            .catch(err =>{
+                response.status(500).json({success:false, err});
+            });
+
+ });
+
+ server.delete('/users/:id', (request, response) => {
+            const {id} = request.params;
+            db.remove(id)
+            .then(deleted => {
+                if(deleted) {
+                    response.status(204).end();
+                }else{
+                    response.status(404).json({success:false, message:'the user you are looking for is not here'})
+                }
+            })
+            .catch(err => {
+                response.status(500).json({success:false, err})
+            });
+        });
+
+
+ server.put('/users/:id', (request, response) => {
+                const {id} = request.params;
+                const userInfo = request.body;
+            const {name, bio} = userInfo
+
+            if(name && bio){
+                db.update(id ,userInfo)
+
+                .then(updated => {
+                    if (updated){
+                    response.status(200).json(userInfo);
+                    }else {response.status(404).json({success:false, message:'the user you are looking for is not here'})
+                }
+                })
+                .catch(err => {
+                    response.status(500).json({success:false, err})
+                });
+            }
+
+                else {
+                    res
+                      .status(400)
+                      .json({ errorMessage: "Please provide name and bio for the user." });
+                  }
+                });
 
  
+server.get('/users/:id', (request, response) => {
+    const {id} = request.params;
+    db.findById(id)
+    .then(user => {
+        response.status(200).json(user)
+
+    })
+    .catch(err => {
+        response.status(500).json({success:false, err})
+    });
+
+})
+
+
 
 server.listen(4000, ()=> {
 
